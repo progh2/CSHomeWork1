66583aa54746b716ea5afe4fbf27ae866770cc40
Your Name Here <progh2@gmail.com>
2019-12-30 10:06:08 +0900

    초기 커밋.

---
+++ HomeWork/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace HomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblResult.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblResult.Location = new System.Drawing.Point(0, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(49, 16);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "label1";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSort.Location = new System.Drawing.Point(0, 59);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(313, 42);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort!!";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.BtnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 101);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ HomeWork/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HomeWork
+{
+    public partial class Form1 : Form
+    {
+        int[] myIntArray = new int[10];
+        Random r = new Random();
+        String result = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                this.myIntArray[i] = r.Next(100);
+            }
+
+            foreach (int num in myIntArray)
+            {
+                result = result + " " + num;
+            }
+
+            lblResult.Text = result;
+        }
+
+        private void BtnSort_Click(object sender, EventArgs e)
+        {
+            Array.Sort(myIntArray);
+            result = "";
+            foreach (int num in myIntArray)
+            {
+                result = result + " " + num;
+            }
+
+            lblResult.Text = result;
+        }
+    }
+}
